e4ac11aab04ab64161f8499db8770dc2347572fa
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2020-05-09 23:00:07 -0700

    Cell: remove unused code

--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -1,17 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-cell',
   templateUrl: './cell.component.html',
   styleUrls: ['./cell.component.scss']
 })
-export class CellComponent implements OnInit {
+export class CellComponent {
   @Input() isAlive = 0;
-  // isAlive = 1;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }

0bd5208a8c8ff2adf977dda2d83e8ab246b7ae94
Carol Diane  St. Louis <stlouis_c@yahoo.com>
2020-05-09 22:44:16 -0700

    added initial grid generation with hard-coded column and row numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-conway';
+export class AppComponent implements OnInit {
+  seedDensity = .25;
+  numRows = 20;
+  numCols = 40;
+  grid: Array<Array<number>> = [];
+
+  ngOnInit() {
+    this.generateGrid();
+  }
+
+  private generateGrid() {
+    const grid: number[][] = [];
+    for (let row = 0; row < this.numRows; row++) {
+      const rowArray: number[] = [];
+      for (let col = 0; col < this.numCols; col ++) {
+        const isLive = Math.floor(Math.random() / this.seedDensity) === 0 ? 1 : 0;
+        rowArray.push(isLive);
+      }
+      grid.push(rowArray);
+    }
+    this.grid = grid;
+  }
 }
